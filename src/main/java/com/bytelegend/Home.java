8f3dab0ff01bbc5a653e8924255dd21353329a0c
AKazaconoks <a.kazaconoks@gmail.com>
2022-09-07 09:39:34 +0000

    Update at 2022-09-07T09:39:34.342033Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -12,6 +12,6 @@ public class Home {
     // the method.
     // Returns that object.
     public static Cat newCat(String name) {
-        return Cat kitty = new Cat (name);
+        return new Cat (name);
     }
 }
