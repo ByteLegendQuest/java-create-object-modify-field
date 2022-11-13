05ce494fd5ddedb5bd8c60dc9cae6bae6c6b836d
wqs111 <wqs820626@qq.com>
2022-11-13 14:41:46 +0000

    Update at 2022-11-13T14:41:46.396299Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -11,5 +11,21 @@ public class Home {
     // Updates the `name` instance field (aka. member variable) of that object to `name` argument of
     // the method.
     // Returns that object.
-    public static Cat newCat(String name) {}
+    public static Cat newCat(String name) {
+          string name;
+          public void getname(){
+                this.name=name;
+
+          }
+
+
+
+
+
+
+
+
+
+
+    }
 }
