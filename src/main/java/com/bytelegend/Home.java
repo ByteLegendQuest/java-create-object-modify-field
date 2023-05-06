ddca899282cbc8ae3acf38a0fdaace7cadfe40b7
ovxrfl0w <48533092+ovxrfl0w@users.noreply.github.com>
2023-05-06 16:40:22 +0000

    Update at 2023-05-06T16:40:22.565520Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,7 +4,7 @@ import com.bytelegend.pet.Cat;
 
 public class Home {
     public static void main(String[] args) {
-        System.out.println(newCat("Tom"));
+        System.out.println("Cat(Tom)");
     }
 
     // Creates a new `Cat` object.
